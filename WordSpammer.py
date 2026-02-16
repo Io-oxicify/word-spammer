833ea19e9d628ab5e0f3bd61cfd81d39e821bbc8
Oxic <pipatalt@gmail.com>
2026-02-17 06:22:55 +0700

    added anti stupidity

--- WordSpammer.py
+++ WordSpammer.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function  
 
 import time
@@ -11,6 +10,14 @@ except NameError:
 print("word spammer by Io")
 w = input_func("give me a word or anything to spam: ")
 t = input_func("Delay? 0 means no delay.")
+
+try:
+    d = (float(t))
+except ValueError:
+    print("this aint a valid number, defaulting to 0.")
+    t = 0
+    time.sleep(1)
+
 while True:
     print(w)
     time.sleep(float(t))
